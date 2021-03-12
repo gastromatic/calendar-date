641c7a8b4da2ae17b6fe24bc46d318c0b80f2306
patrick-kle1n <p.klein@gastromatic.de>
2021-03-12 16:21:49 +0100

    feat: add CalendarDate

---
+++ test/helpers.ts
@@ -0,0 +1,7 @@
+import { CalendarDate } from 'calendar-date';
+
+export const DAY_IN_SECONDS = 86400;
+
+export function ensureValidDay(year: number, month: number, day: number): number {
+  return Math.max(1, Math.min(day, CalendarDate.getMaxDayOfMonth(year, month)));
+}
